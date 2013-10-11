91be4fa675f7acdf43b912019d258f140618c391
Hidenori Miyoshi <miyohide@gmail.com>
2013-10-11 22:44:56 +0900

    Application name change. blog -> bloog

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '2fde9f59e1fa4f680693ce4c6a5885b25a566a388ffe82d19fec5c5364c700743093555e139a3b8b8cc950ea30f59f1402377794676f4a0b2b3a66a1e37470f9'
+Bloog::Application.config.secret_token = '2fde9f59e1fa4f680693ce4c6a5885b25a566a388ffe82d19fec5c5364c700743093555e139a3b8b8cc950ea30f59f1402377794676f4a0b2b3a66a1e37470f9'
