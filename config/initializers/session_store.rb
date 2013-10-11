453f8fe1d6f1a7233f090ab23720a3fdd5b29848
Hidenori Miyoshi <miyohide@gmail.com>
2013-10-11 22:48:27 +0900

    Application name change. (blog -> bloog) part2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ Bloog::Application.config.session_store :cookie_store, key: '_bloog_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Blog::Application.config.session_store :active_record_store
+# Bloog::Application.config.session_store :active_record_store
