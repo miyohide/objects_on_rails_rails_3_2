f45f689b3a7d08c51d2d65365a82119633772654
Hidenori Miyoshi <miyohide@gmail.com>
2013-10-12 23:38:39 +0900

    Implementing Blog#new_post

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 class Blog
    attr_reader :entries
+   attr_writer :post_source
 
    def initialize
       @entries = []
@@ -12,6 +13,17 @@ class Blog
 
    def subtitle
       "The trusted source for drying paint new & opinion"
+   end
+
+   def new_post
+      post_source.call.tap do |p|
+         p.blog = self
+      end
+   end
+
+   private
+   def post_source
+      @post_source ||= Post.public_method(:new)
    end
 end
 
