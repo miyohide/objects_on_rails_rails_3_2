1c320bb0b1b9772db62760ed93d0a4c44b52a685
Hidenori Miyoshi <miyohide@gmail.com>
2013-10-23 21:10:34 +0900

    add gem 'rr'. 実行する場合はbundle execをつけるとよい

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -1,5 +1,11 @@
 # coding: utf-8
 
+require 'rr'
+
+class MiniTest::Unit::TestCase
+   include RR::Adapters::MiniTest
+end
+
 def stub_module(full_name)
    full_name.to_s.split(/::/).inject(Object) do |context, name|
       begin
