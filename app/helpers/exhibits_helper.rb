f12a347ff1b392ffcb117f17c347a2ae15d0f6a0
Hidenori Miyoshi <miyohide@gmail.com>
2014-01-01 22:34:14 +0900

    Listing 66. Implementing the exhibit helper. ExhibitsHelperの実装。この時点では、stub_classが未実装のためテストが落ちる。

---
+++ app/helpers/exhibits_helper.rb
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+module ExhibitsHelper
+   def exhibit(model, context)
+      case model.class.name
+      when 'Post'
+         if model.picture?
+            PicturePostExhibit.new(model, context)
+         else
+            TextPostExhibit.new(model, context)
+         end
+      else
+         model
+      end
+   end
+end
+
