4ada86ba178b49a223b42c52a616217f5f396f71
Hidenori Miyoshi <miyohide@gmail.com>
2014-01-04 15:12:08 +0900

    Listing 69. Adding the exhibits helper to the ApplicationController. ControllerでもHelperメソッドが使えるように設定。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :init_blog
 
+  # helper method infomation. See below URL.
+  # http://apidock.com/rails/AbstractController/Helpers/ClassMethods/helper
+  helper :exhibits
+
   private
   def init_blog
      @blog = THE_BLOG
