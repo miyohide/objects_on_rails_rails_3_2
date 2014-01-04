dbc25fbcfb75a19537b05f29b34bb70808ebc3ba
Hidenori Miyoshi <miyohide@gmail.com>
2014-01-04 18:31:59 +0900

    Fix Test Fail. テキストのコードを入力した場合にテストが落ちる問題を解消。 テスト実行時に「wrong number of arguments(2 for 0)」と出てテストが2件失敗する。 Ruby 1.9.2とRuby 1.9.3との互換性の問題から派生しているようで、テスト内容を修正した。

--- spec/helpers/exhibits_helper_spec.rb
+++ spec/helpers/exhibits_helper_spec.rb
@@ -12,19 +12,25 @@ describe ExhibitsHelper do
    before do
       @it = Object.new
       @it.extend ExhibitsHelper
+      # Ruby 1.9.2と1.9.3との間での非互換性の関係でテキストにある
+      # 内容ではテストに失敗する。
+      # ここでは、newメソッドをstub化してテスト内容を修正した。
+      # （参考）https://groups.google.com/forum/#!searchin/objects-on-rails/exhibits_helper_spec/objects-on-rails/3272cyA23OY/w1J_GNV5ayMJ
+      stub(PicturePostExhibit).new { "PICTURE_POST_EXHIBIT" }
+      stub(TextPostExhibit).new { "TEXT_POST_EXHIBIT" }
       @context = stub!
    end
 
    it "decorates picture posts with a PicturePostExhibit" do
       post = Post.new
       stub(post).picture? { true }
-      @it.exhibit(post, @context).must_be_kind_of(PicturePostExhibit)
+      @it.exhibit(post, @context).must_equal "PICTURE_POST_EXHIBIT"
    end
 
    it "decorates text posts with a TextPostExhibit" do
       post = Post.new
       stub(post).picture? { false }
-      @it.exhibit(post, @context).must_be_kind_of(TextPostExhibit)
+      @it.exhibit(post, @context).must_equal "TEXT_POST_EXHIBIT"
    end
 
    it "leaves objects it doesn't know about alone" do
