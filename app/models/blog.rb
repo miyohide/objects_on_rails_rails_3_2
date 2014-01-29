f190dc28e7268040eca24cd9059c10e740fb3193
Hidenori Miyoshi <miyohide@gmail.com>
2014-01-29 23:19:33 +0900

    Listing 87. Updating Blog to use ActiveRecord. この修正の時点でspecは落ちる。

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,9 @@
 # coding: utf-8
 class Blog
-   attr_reader :entries
    attr_writer :post_source
 
-   def initialize
-      @entries = []
+   def initialize(entry_fetcher=Post.public_method(:all))
+      @entry_fetcher = entry_fetcher
    end
 
    def title
@@ -22,16 +21,20 @@ class Blog
    end
 
    def add_entry(entry)
-      @entries << entry
+      entry.save
    end
 
    def entries
-      @entries.sort_by { |e| e.pubdate }.reverse.take(10)
+      fetch_entries.sort_by { |e| e.pubdate }.reverse.take(10)
    end
 
    private
    def post_source
       @post_source ||= Post.public_method(:new)
+   end
+
+   def fetch_entries
+      @entry_fetcher.()
    end
 end
 
