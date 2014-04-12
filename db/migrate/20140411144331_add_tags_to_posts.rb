c067e16f749b980ec49cd9c275487a610d9d2ad9
Hidenori Miyoshi <miyohide@gmail.com>
2014-04-12 19:03:24 +0900

    Listing 127. A migration to add tags to posts.

---
+++ db/migrate/20140411144331_add_tags_to_posts.rb
@@ -0,0 +1,10 @@
+class AddTagsToPosts < ActiveRecord::Migration
+   def self.up
+      add_column :posts, :tags, :string
+   end
+
+   def self.down
+      remove_column :posts, :tags, :string
+   end
+end
+
