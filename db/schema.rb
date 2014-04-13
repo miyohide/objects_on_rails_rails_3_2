9a19009fcaaba5f72b5a1cd04e5168c1041ab697
Hidenori Miyoshi <miyohide@gmail.com>
2014-04-13 21:59:59 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 20140411144331) do
 
   create_table "posts", :force => true do |t|
     t.datetime "pubdate"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 1) do
     t.string   "image_url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "tags"
   end
 
 end
