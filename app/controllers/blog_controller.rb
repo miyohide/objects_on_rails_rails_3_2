86f6a5bbc3b4748413c90b6a840b34769c924444
Hidenori Miyoshi <miyohide@gmail.com>
2014-04-18 22:23:45 +0900

    Listing 135. Enabling the front page to be filtered by tag.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,8 @@
 # coding: utf-8
 class BlogController < ApplicationController
    def index
+      if params[:tag].present?
+         @blog = @blog.filter_by_tag(params[:tag])
+      end
    end
 end
