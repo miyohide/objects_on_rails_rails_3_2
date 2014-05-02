42939e3a5b9c683854ecd440131d2ca50ea7470e
Hidenori Miyoshi <miyohide@gmail.com>
2014-05-02 23:18:55 +0900

    Remove old tagged method.

--- app/models/taggable_relation.rb
+++ app/models/taggable_relation.rb
@@ -11,10 +11,6 @@ module TaggableRelation
       TagList(ItemTag.where(item_type: klass).includes(:tag).map(&:name))
    end
 
-   def tagged(tag)
-      select { |e| Taggable(e).tags.to_a.include?(tag) }
-   end
-
    def new(attrs={}, &block)
       attrs = attrs.dup
       tags = attrs.delete(:tags)
