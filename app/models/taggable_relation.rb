1ad44b576675be50a5e123fe261fe80ef4307591
Hidenori Miyoshi <miyohide@gmail.com>
2014-05-02 22:56:21 +0900

    Listing 161. Implementing TaggableRelation#new to handle 'tags' attribute.

--- app/models/taggable_relation.rb
+++ app/models/taggable_relation.rb
@@ -14,5 +14,13 @@ module TaggableRelation
    def tagged(tag)
       select { |e| Taggable(e).tags.to_a.include?(tag) }
    end
+
+   def new(attrs={}, &block)
+      attrs = attrs.dup
+      tags = attrs.delete(:tags)
+      Taggable(super(attrs, &block)).tap do |item|
+         item.tags = tags
+      end
+   end
 end
 
