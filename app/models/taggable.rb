0b5d867db0bb1f36cd14824e76bf492bd67528ec
Hidenori Miyoshi <miyohide@gmail.com>
2014-05-02 22:42:55 +0900

    Listing 159. Updating Taggable() to use TagStorage

--- app/models/taggable.rb
+++ app/models/taggable.rb
@@ -8,14 +8,16 @@ module Taggable
 end
 
 module Conversions
-   def Taggable(item)
+   def Taggable(item, tag_storage=::TagStorage.new(item))
+      return item if item.kind_of?(::Taggable)
       case item
       when ::Class, ::ActiveRecord::Relation
          item.extend(::TaggableRelation)
       else
          item.extend(::TaggableRecord)
+         item._tag_storage = tag_storage
       end
-      item.extend(::Taggable)
+      item
    end
 end
 
