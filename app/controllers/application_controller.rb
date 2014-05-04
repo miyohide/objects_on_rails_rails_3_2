6259d74695b6297dd8b7f74f972a4d437f1f5313
Hidenori Miyoshi <miyohide@gmail.com>
2014-05-04 22:05:25 +0900

    Listing 164. Exposing ApplicationController#blog to views with helper_method. And remove init_blog method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :init_blog
 
   # helper method infomation. See below URL.
   # http://apidock.com/rails/AbstractController/Helpers/ClassMethods/helper
@@ -11,11 +10,8 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def init_blog
-     @blog = THE_BLOG
-  end
-
   def blog
      @blog ||= THE_BLOG
   end
+  helper_method :blog
 end
