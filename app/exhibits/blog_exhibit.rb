cfdb90eb7966eeae7c97408991ca8debb279bc8b
Hidenori Miyoshi <miyohide@gmail.com>
2014-06-24 23:09:12 +0900

    Listing 180. BlogExhibit applicability.

--- app/exhibits/blog_exhibit.rb
+++ app/exhibits/blog_exhibit.rb
@@ -3,6 +3,10 @@
 require_relative 'exhibit'
 
 class BlogExhibit < Exhibit
+   def self.applicable_to?(object)
+      object_is_any_of?(object, 'Blog', 'Blog::FilteredBlog')
+   end
+
    def filter_by_tag(*)
       exhibit(super)
    end
