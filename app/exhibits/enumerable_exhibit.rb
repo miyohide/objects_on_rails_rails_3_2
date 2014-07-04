701765873051b3d892456529b89c147506034ad7
Hidenori Miyoshi <miyohide@gmail.com>
2014-07-04 22:45:27 +0900

    Listing 196. Implementing EnumerableExhibit#render to render each element.

--- app/exhibits/enumerable_exhibit.rb
+++ app/exhibits/enumerable_exhibit.rb
@@ -39,5 +39,11 @@ class EnumerableExhibit < Exhibit
    def to_ary
       self
    end
+
+   def render(template)
+      inject(ActiveSupport::SafeBuffer.new) { |output, element|
+         output << element.render(template)
+      }
+   end
 end
 
