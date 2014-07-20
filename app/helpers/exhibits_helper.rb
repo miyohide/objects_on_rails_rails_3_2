d10acf29e645dc89ae91b5dfcb443d841eca3653
Hidenori Miyoshi <miyohide@gmail.com>
2014-07-20 23:03:57 +0900

    set default context

--- app/helpers/exhibits_helper.rb
+++ app/helpers/exhibits_helper.rb
@@ -3,7 +3,7 @@
 require_relative '../../app/exhibits/exhibit'
 
 module ExhibitsHelper
-   def exhibit(model, context)
+   def exhibit(model, context = self)
       Exhibit.exhibit(model, context)
    end
 end
